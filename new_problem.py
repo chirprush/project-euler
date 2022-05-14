5dee61e21274586e3d7e37d72d0190b51809bbdd
chirprush <chirpyboat73@gmail.com>
2022-05-13 20:21:51 -0400

    Specify a problem by number

--- new_problem.py
+++ new_problem.py
@@ -9,16 +9,26 @@ def assert_args(cond: bool, message: str) -> None:
         print(f"new_problem.py: {message}", file=stderr)
         exit(-1)
 
-assert_args(len(argv) == 2, "Expected a template directory to use")
+assert_args(len(argv) in range(2, 4), "Expected a template directory to use")
 
 template = Path(argv[1])
 template_path = Path("./templates").joinpath(template)
 
 assert_args(template_path.exists(), f"Template directory '{argv[1]}' does not exist")
 
-last_problem = max([int(path.name) for path in Path("./problems").iterdir() if path.name.isnumeric()], default=0)
-new_problem = Path(str(last_problem + 1))
+if len(argv) == 3 and argv[2].isnumeric():
+    new_problem = Path(argv[2])
+else:
+    current_problems = sorted([int(path.name) for path in Path("./problems").iterdir() if path.name.isnumeric()])
+    last_consecutive = 1
+    for problem in current_problems:
+        if last_consecutive == problem:
+            last_consecutive += 1
+    new_problem = Path(str(last_consecutive))
+
 new_problem_path = Path("./problems").joinpath(new_problem)
+
+assert_args(not new_problem_path.exists(), f"Problem directory '{str(new_problem)}' already exists")
 
 copytree(str(template_path), str(new_problem_path))
 
